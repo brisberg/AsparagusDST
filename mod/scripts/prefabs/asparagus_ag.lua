f6652c3515a0e928146167726a1242477e9d2ca5
Brandon Risberg <brandon.risberg@gmail.com>
2017-12-08 21:32:36 -0800

    Rebuilt the spriter project such that it successfully renders in game

--- mod/scripts/prefabs/asparagus_ag.lua
+++ mod/scripts/prefabs/asparagus_ag.lua
@@ -16,7 +16,7 @@ local function fn(Sim)
 
   MakeInventoryPhysics(inst)
 
-  inst.AnimState:SetBank("sprites")
+  inst.AnimState:SetBank("asparagus_ag_anim")
   inst.AnimState:SetBuild("asparagus_ag_build")
   inst.AnimState:PlayAnimation("idle", true)
 
@@ -33,19 +33,19 @@ end
 
 
 -- Add some strings for this item
-STRINGS.NAMES.BINDINGRING = "Asparagus"
+STRINGS.NAMES.ASPARAGUS_AG = "Asparagus name"
 
 --Strings courtesy of The Silent Dapper Darkness
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.BINDINGRING = "I can use this to join my friend."
-STRINGS.CHARACTERS.WILLOW.DESCRIBE.BINDINGRING = "It's so shiny. Almost as pretty as a fire."
-STRINGS.CHARACTERS.WOLFGANG.DESCRIBE.BINDINGRING = "Wolfgang does not wear jewellery. Could help Wolfgang, however?"
-STRINGS.CHARACTERS.WENDY.DESCRIBE.BINDINGRING = "I can use this to return to the one person who understands my pain."
-STRINGS.CHARACTERS.WX78.DESCRIBE.BINDINGRING = "RELOCATION DEVICE. THE POWER SOURCE IS A MYSTERY."
-STRINGS.CHARACTERS.WICKERBOTTOM.DESCRIBE.BINDINGRING = "I've not done any research to back this up, but I have a hunch about this ring."
-STRINGS.CHARACTERS.WOODIE.DESCRIBE.BINDINGRING = "It's like one of those tree rings, eh? But shinier. I bet Lucy would have loved this."
-STRINGS.CHARACTERS.WAXWELL.DESCRIBE.BINDINGRING = "Now if only this ring would get me out of here."
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.ASPARAGUS_AG = "I can use this to join my friend."
+STRINGS.CHARACTERS.WILLOW.DESCRIBE.ASPARAGUS_AG = "It's so shiny. Almost as pretty as a fire."
+STRINGS.CHARACTERS.WOLFGANG.DESCRIBE.ASPARAGUS_AG = "Wolfgang does not wear jewellery. Could help Wolfgang, however?"
+STRINGS.CHARACTERS.WENDY.DESCRIBE.ASPARAGUS_AG = "I can use this to return to the one person who understands my pain."
+STRINGS.CHARACTERS.WX78.DESCRIBE.ASPARAGUS_AG = "RELOCATION DEVICE. THE POWER SOURCE IS A MYSTERY."
+STRINGS.CHARACTERS.WICKERBOTTOM.DESCRIBE.ASPARAGUS_AG = "I've not done any research to back this up, but I have a hunch about this ring."
+STRINGS.CHARACTERS.WOODIE.DESCRIBE.ASPARAGUS_AG = "It's like one of those tree rings, eh? But shinier. I bet Lucy would have loved this."
+STRINGS.CHARACTERS.WAXWELL.DESCRIBE.ASPARAGUS_AG = "Now if only this ring would get me out of here."
 
-STRINGS.RECIPE_DESC.BINDINGRING = "Binds to your soul."
+STRINGS.RECIPE_DESC.ASPARAGUS_AG = "Binds to your soul."
 
 -- Finally, return a new prefab with the construction function and assets.
 return Prefab( "common/inventory/asparagus_ag", fn, Assets)
