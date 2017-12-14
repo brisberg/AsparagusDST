a76028b757ce5c0bc42ab63e01a81a9da968310e
Brandon Risberg <brandon.risberg@gmail.com>
2017-12-13 22:52:15 -0800

    better, added a new state for sharing which leads to quickeat

--- mod/modmain.lua
+++ mod/modmain.lua
@@ -35,16 +35,57 @@ SHARE.fn = function(act)
 	-- if targ.components.watch then
 	-- 	return act.doer.components.predictor:Predict(targ)
 	-- end
+	-- act.doer.sg:GoToState("quickeat", {feed=act.invobject, feeder=act.doer})
 	print("SHARE action")
 end
 AddAction(SHARE)
-AddStategraphActionHandler("wilson", ActionHandler(SHARE, "book"))
+AddStategraphActionHandler("wilson", ActionHandler(SHARE, "share"))
 
 local function share_item(inst, doer, target, actions, right)
 	print("checking share_item")
   -- Do logic here to check if we should enable the actions
-	-- Use doer.replica and doer:hasTag("foo") for testing, not the
+	-- Use doer.replica and doer:HasTag("foo") for testing, not the
 	-- components directly
-	table.insert(actions, GLOBAL.ACTIONS.SHARE)
+	if right then
+		table.insert(actions, GLOBAL.ACTIONS.SHARE)
+	end
 end
 AddComponentAction("USEITEM", "sharable", share_item)
+
+-- States
+local State = GLOBAL.State
+local TimeEvent = GLOBAL.TimeEvent
+local EventHandler = GLOBAL.EventHandler
+local FRAMES = GLOBAL.FRAMES
+
+local share_sg = State({
+  name = "share",
+  tags = { "giving" },
+
+  onenter = function(inst)
+    inst.components.locomotor:Stop()
+		inst.sg.statemem.shareitem = inst:GetBufferedAction().invobject
+    inst.AnimState:PlayAnimation("give")
+    inst.AnimState:PushAnimation("give_pst", false)
+  end,
+
+  timeline =
+  {
+    TimeEvent(13 * FRAMES, function(inst)
+    	inst:PerformBufferedAction()
+    end),
+  },
+
+  events =
+  {
+    EventHandler("animqueueover", function(inst)
+      if inst.AnimState:AnimDone() then
+        inst.sg:GoToState(
+					"quickeat",
+					{feed=inst.sg.statemem.shareitem, feeder=inst}
+				)
+      end
+    end),
+  },
+})
+AddStategraphState("wilson", share_sg)
