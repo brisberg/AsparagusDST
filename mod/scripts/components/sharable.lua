b635eced8d22147e0e73de8a2bba8f2f8dee4be2
Brandon Risberg <brandon.risberg@gmail.com>
2017-12-18 22:10:19 -0800

    Added a few safty checks

--- mod/scripts/components/sharable.lua
+++ mod/scripts/components/sharable.lua
@@ -1,9 +1,10 @@
 local Sharable = Class(function(self, inst)
+    -- inst should have an edible component
     self.inst = inst
 end)
 
 function Sharable:SpawnHalfItemToken()
-  if self.inst.components.edible then
+  if self.inst and self.inst.components.edible then
     local halffood = SpawnPrefab(self.inst.prefab)
     halffood.components.edible.healthvalue = halffood.components.edible.healthvalue/2
     halffood.components.edible.hungervalue = halffood.components.edible.hungervalue/2
