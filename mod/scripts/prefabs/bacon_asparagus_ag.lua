bdb8b5a469a366f496d332e7dce08078a401fe94
Brandon Risberg <brandon.risberg@gmail.com>
2018-01-02 19:06:17 -0800

    Added spriter project for bacon asparagus along with its recipe

--- mod/scripts/prefabs/bacon_asparagus_ag.lua
+++ mod/scripts/prefabs/bacon_asparagus_ag.lua
@@ -1,7 +1,9 @@
+local recipe = require("tweaks/bacon_asparagus_recipe")
+
 local Assets =
 {
-	Asset("ANIM", "anim/cook_pot_food.zip"), -- a standard asset
-	-- Asset("ATLAS", "images/inventoryimages/asparagus_ag.xml"),    -- a custom asset, found in the mod folder
+	Asset("ANIM", "anim/bacon_asparagus_ag.zip"), -- a standard asset
+	Asset("ATLAS", "images/inventoryimages/bacon_asparagus_ag.xml"),    -- a custom asset, found in the mod folder
 }
 
 local function fn()
@@ -13,9 +15,9 @@ local function fn()
 
     MakeInventoryPhysics(inst)
 
-    inst.AnimState:SetBuild("cook_pot_food")
-    inst.AnimState:SetBank("food")
-    inst.AnimState:PlayAnimation("waffles", false)
+		inst.AnimState:SetBuild("bacon_asparagus_ag")
+	  inst.AnimState:SetBank("bacon_asparagus_ag_anim")
+	  inst.AnimState:PlayAnimation("idle")
 
     inst:AddTag("preparedfood")
     -- if data.tags then
@@ -31,25 +33,26 @@ local function fn()
     end
 
     inst:AddComponent("edible")
-    inst.components.edible.healthvalue = data.health
-    inst.components.edible.hungervalue = data.hunger
-    inst.components.edible.foodtype = data.foodtype or FOODTYPE.GENERIC
-    inst.components.edible.sanityvalue = data.sanity or 0
-    inst.components.edible.temperaturedelta = data.temperature or 0
-    inst.components.edible.temperatureduration = data.temperatureduration or 0
-    inst.components.edible:SetOnEatenFn(data.oneatenfn)
+    inst.components.edible.healthvalue = recipe.health
+    inst.components.edible.hungervalue = recipe.hunger
+    inst.components.edible.foodtype = recipe.foodtype or FOODTYPE.GENERIC
+    inst.components.edible.sanityvalue = recipe.sanity or 0
+    inst.components.edible.temperaturedelta = recipe.temperature or 0
+    inst.components.edible.temperatureduration = recipe.temperatureduration or 0
+    inst.components.edible:SetOnEatenFn(recipe.oneatenfn)
 
     inst:AddComponent("inspectable")
-    inst.wet_prefix = data.wet_prefix
+    inst.wet_prefix = recipe.wet_prefix
 
     inst:AddComponent("inventoryitem")
+		inst.components.inventoryitem.atlasname = "images/inventoryimages/bacon_asparagus_ag.xml"
 
     inst:AddComponent("stackable")
     inst.components.stackable.maxsize = TUNING.STACK_SIZE_SMALLITEM
 
-    if data.perishtime ~= nil and data.perishtime > 0 then
+    if recipe.perishtime ~= nil and recipe.perishtime > 0 then
     	inst:AddComponent("perishable")
-    	inst.components.perishable:SetPerishTime(data.perishtime)
+    	inst.components.perishable:SetPerishTime(recipe.perishtime)
     	inst.components.perishable:StartPerishing()
     	inst.components.perishable.onperishreplacement = "spoiled_food"
     end
@@ -67,5 +70,11 @@ local function fn()
 
     return inst
 end
+
+-- Add some strings for this item
+STRINGS.NAMES.BACON_ASPARAGUS_AG = "Bacon-wrapped Asparagus"
+
+--Strings courtesy of The Silent Dapper Darkness
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.BACON_ASPARAGUS_AG = "Juicy bacon asparagus skewers."
 
 return Prefab("common/inventory/bacon_asparagus_ag", fn, Assets)
--- mod/scripts/tweaks/bacon_asparagus_recipe.lua
+++ mod/scripts/tweaks/bacon_asparagus_recipe.lua
@@ -1,21 +1,29 @@
-local FOODTYPE = GLOBAL.FOODTYPE
+-- local FOODTYPE = GLOBAL.FOODTYPE
+--
+-- local bacon_asparagus_recipe = {
+-- 	name = "bacon_asparagus_ag",
+-- 	test = function(cooker, names, tags) return (names.asparagus_ag or names.asparagus_ag_cooked) and names.twigs and (tags.meat and tags.meat >= 1) end,
+-- 	priority = 10,
+-- 	weight = 1,
+-- 	foodtype = FOODTYPE.MEAT,
+-- 	health = TUNING.HEALING_HUGE,
+-- 	hunger = TUNING.CALORIES_LARGE,
+-- 	perishtime = TUNING.PERISH_FAST,
+-- 	sanity = TUNING.SANITY_TINY,
+-- 	cooktime = .5,
+-- }
 
 local bacon_asparagus_recipe = {
 	name = "bacon_asparagus_ag",
 	test = function(cooker, names, tags) return (names.asparagus_ag or names.asparagus_ag_cooked) and names.twigs and (tags.meat and tags.meat >= 1) end,
 	priority = 10,
 	weight = 1,
-	foodtype = FOODTYPE.MEAT,
-	health = TUNING.HEALING_HUGE,
-	hunger = TUNING.CALORIES_LARGE,
-	perishtime = TUNING.PERISH_FAST,
-	sanity = TUNING.SANITY_TINY,
+	foodtype = "MEAT",
+	health = 40,
+	hunger = 30,
+	perishtime = 300,
+	sanity = 5,
 	cooktime = .5,
 }
-
-AddCookerRecipe("cookpot", bacon_asparagus_recipe)
-
--- Make asparagus a vegetable in the crockpot
-AddIngredientValues({"asparagus_ag"}, {veggie=1}, true)
 
 return bacon_asparagus_recipe
